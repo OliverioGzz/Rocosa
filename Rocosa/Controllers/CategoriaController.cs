81d43c3c4d9657933a777c3543213b13e5b2ab11
Oliverio Gzz <oliverio.gzz@outlook.com>
2022-06-11 10:11:31 -0500

    Agregar archivos de proyecto.

---
+++ Rocosa/Controllers/CategoriaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocosa.Datos;
+using Rocosa.Models;
+
+namespace Rocosa.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = _db.Categoria;
+            return View(lista);
+        }
+    }
+}
